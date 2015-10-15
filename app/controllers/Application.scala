04916e4cec97d851ed8982ad4dd4aef77f6e6f30
Sven Hodapp <sven.hodapp@gmail.com>
2015-10-15 09:59:17 +0200

    First working example, but without persistence but good performance

---
+++ app/controllers/Application.scala
@@ -0,0 +1,83 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+import org.fusesource.stomp.jms._
+import javax.jms._
+
+import scala.concurrent.duration._
+import scala.concurrent.{ Future, Promise }
+import scala.concurrent.Await
+import scala.concurrent.TimeoutException
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import scala.collection.JavaConverters._
+
+import collection.mutable.{ HashMap, MultiMap, Set, ListBuffer }
+
+import org.fusesource.stomp.client.Constants._
+import org.fusesource.stomp.codec.StompFrame
+import org.fusesource.stomp.client.Stomp
+
+class Application extends Controller {
+
+  // Simple data store (TODO: persist to simple csv flat file, leveldb, h2, accumulo, https://github.com/jeluard/stone, https://github.com/rrd4j/rrd4j?)
+  var head = List[Tuple6[String, String, Long, String, String, Double]]()
+
+  // in future, make for each message-collector a extra list.
+  // merge the list.views for the analysis view to get a global view.
+  // make the view a parallel collection with .par for better performance.
+  // how far can we go with such a simple concept?
+  def analysisView = head.view.par
+
+  var ordererAgentSet = Set[String]()
+
+  // Connect to the broker with a raw connection
+  val stomp = new Stomp("ashburner", 61613)
+  val connection = stomp.connectBlocking
+
+  // Listen on the billing topic
+  val frame = new StompFrame(SUBSCRIBE)
+  frame.addHeader(DESTINATION, StompFrame.encodeHeader("/topic/billing"))
+  frame.addHeader(ID, connection.nextId)
+  val response = connection.request(frame)
+
+  // fetch all messages
+  Future {
+    while (true) {
+      val frame = connection.receive
+      if (frame.action == MESSAGE) {
+        val headerList = frame.headerList.asScala
+        val header = headerList.map( x =>
+          x.getKey.toString -> x.getValue.toString
+        ).toMap
+
+        val (orderNr, ordererAgent) = header("tracking-nr").split("@") match {
+          case Array(orderNr, ordererAgent) => (orderNr, ordererAgent)
+          case _ => ("undefiend", "undefined")
+        }
+
+        val tuple = (
+          ordererAgent, orderNr,
+          header("timestamp").toLong, header("event"),
+          header("agent"), 0.01 * header("unit").toDouble
+        )
+
+        ordererAgentSet = Set(ordererAgent) ++ ordererAgentSet
+
+        head = tuple :: head  // This is O(1)
+        println(tuple)
+      }
+    }
+  }
+
+  def index = Action {
+    val stats = ordererAgentSet.map( userKey =>
+      userKey -> analysisView.filter(_._1 == userKey).map(_._6).sum
+    ).toMap
+
+    Ok(views.html.main("Accounting")(head.size, stats))
+  }
+
+}
---
+++ write_mock.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+"""
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import os
+import sys
+import time
+import signal
+import uuid
+import random
+
+
+# pip install stompest==2.1.6
+from stompest.config import StompConfig
+from stompest.sync import Stomp
+
+
+user = os.getenv('APOLLO_USER') or 'admin'
+password = os.getenv('APOLLO_PASSWORD') or 'password'
+host = os.getenv('APOLLO_HOST') or 'ashburner'
+port = int(os.getenv('APOLLO_PORT') or 61613)
+destination = sys.argv[1:2] or ['/topic/billing']
+destination = destination[0]
+
+config = StompConfig('tcp://%s:%d' % (host, port), login=user, passcode=password, version='1.1')
+client = Stomp(config)
+client.connect(host=host)
+
+count = 0
+start = time.time()
+
+def signal_handler(signal, frame):
+    print('You pressed Ctrl+C!')
+    diff = time.time() - start
+    print 'Received %s frames in %f seconds' % (count, diff)
+    client.disconnect(receipt='bye')
+    client.receiveFrame()
+    client.close()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+while True:
+    #time.sleep(1)  # delay in secs
+    body = []
+    body.append("Not needed...")
+    users = {0: "einstein", 1: "newton", 2: "anonymous"}
+    headers = {
+        'transformation': 'TEXT',
+        'event': 'MockQuery',
+        'agent': 'MockInstance',
+        'tracking-nr': str(uuid.uuid4()) + "@" + users[random.randrange(3)],
+        'timestamp': int(time.time()),
+        'unit': random.randrange(9) + 1
+    }
+    print "Sending to " + destination
+    client.send(destination=destination, body=body[0], headers=headers)
+    count = count + 1
